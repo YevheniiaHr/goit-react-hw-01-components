748fa24b5d28b2dbb014341f31fd020c4e280fb9
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2023-10-28 15:47:13 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,14 @@
 import user from './Profile/user.json';
 import Profile from './Profile/Profile';
+
+import data from './Statistics/data.json';
+import Statistics from './Statistics/Statistics';
+
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
+
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
 export default function App(){
     return ( 
        <div>
@@ -10,7 +19,13 @@ export default function App(){
          avatar={user.avatar}
          stats={user.stats}
         />
+         <Statistics title="Upload stats" stats={data}/>
+         <FriendList friends={friends}/>
+         <TransactionHistory items={transactions}/>
+       
        </div> 
+       
+       
     );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,46 +1,56 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import {
+  Avatar,
+  Description,
+  ListItem,
+  ProfileContainer,
+  Stats,
+  Label,
+  Quantity,
+  Name,
+  Tag,
+  Location,
+} from './Profile.styled';
 
 const Profile = ({ username, tag, location, avatar, stats }) => {
-    return (<div className="profile">
-    <div className="description">
-      <img
-        src={avatar}
-        alt={username}
-        className="avatJacques GlukeaJacques Gluker"
-      />
-      <p className="name">{username}</p>
-      <p className="tag">{tag}</p>
-      <p className="location">{location}</p>
-    </div>
-    
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity"> {stats.followers}</span>
-      </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity"> {stats.views}</span>
-      </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity"> {stats.likes}</span>
-      </li>
-    </ul>
-    </div>);
+  return (
+    <ProfileContainer>
+      <Description>
+        <Avatar src={avatar} alt={username} />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <Stats>
+        <ListItem>
+          <Label>Followers</Label>
+          <Quantity> {stats.followers}</Quantity>
+        </ListItem>
+        <ListItem>
+          <Label>Views</Label>
+          <Quantity> {stats.views}</Quantity>
+        </ListItem>
+        <ListItem>
+          <Label>Likes</Label>
+          <Quantity> {stats.likes}</Quantity>
+        </ListItem>
+      </Stats>
+    </ProfileContainer>
+  );
 };
 
 Profile.propTypes = {
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    stats: PropTypes.arrayOf(
-        PropTypes.shape({
-            followers: PropTypes.number.isRequired,
-            views: PropTypes.number.isRequired,
-            likes: PropTypes.number.isRequired,
-        }).isRequired
-    ).isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape(
+    {
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }.isRequired
+  ).isRequired,
 };
 export default Profile;
